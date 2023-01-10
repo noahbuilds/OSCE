586b6d6254f49accab2ce631eb53dc9ad9092790
noahbuilds <edetnoah@gmail.com>
2023-01-10 16:58:38 +0100

    feat: implements new update

---
+++ src/app/pages/examiner/monitor/monitor-procedure-table/monitor-procedure-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-monitor-procedure-table',
+  templateUrl: './monitor-procedure-table.component.html',
+  styleUrls: ['./monitor-procedure-table.component.scss']
+})
+export class MonitorProcedureTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
